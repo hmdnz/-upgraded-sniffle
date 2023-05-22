c65f3f19ee842bd4dc723eb62f103b9b4d136f5a
hmdnz <hammadmcferri@gmail.com>
2023-05-22 22:30:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,17 @@
-body {
-    background-image: url("station.jpg");
-    background-size: cover;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    font-weight: bold;
-    text-align: center;
+// grab the count-el element, store it in a countEl variable
+
+
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
 }
 
-h1 {
-    margin-top: 5px;
-    margin-bottom: 0px;
-}
-
-h2 {
-    font-size: 50px;
-    margin-top: 0;
-    margin-bottom: 10px;
-}
-
-button {
-    border: none;
-    padding-top: 10px;
-    padding-bottom: 10px;
-    color: white;
-    font-weight: bold;
-    width: 200px;
-    margin-bottom: 5px;
-    border-radius: 5px;
-}
-
-#increment-btn {
-    background: greenyellow;
-}
-
-#decrement-btn {
-    background: lightcoral;
-}
-
-#save-btn {
-    background: darkgreen;
-}
-
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
